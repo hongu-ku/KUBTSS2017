1e7a1b15a8704fec031b9ed0383277877d652603
hongu-ku <hongu.sol.0901@gmail.com>
2017-04-26 23:10:43 +0900

    apaapa by hongu

--- app/src/main/java/com/example/hongu/apaapa/MapsActivity.java
+++ app/src/main/java/com/example/hongu/apaapa/MapsActivity.java
@@ -662,7 +662,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //                    }
 //                });
                     try {
-                        Thread.sleep(1000);
+                        Thread.sleep(500);
                     } catch (InterruptedException e) {
                         Log.e(TAG, "CloudLoggerSendThread exception");
                     }
